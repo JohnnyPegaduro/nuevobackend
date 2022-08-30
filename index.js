bfcc924b3c29c8551c6f1522af7767ddf355a117
JohnnyPegaduro <matiasdeluca2000@gmail.com>
2022-08-30 18:46:15 -0300

    desafio 3 y dependencias

--- index.js
+++ index.js
@@ -1,144 +1,42 @@
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const Container = require("./container");
+const contenedor = new Container("./products.json");
 
-class Container {
-    constructor(file) {
-        this.file = file;
-    }
+//? Settings
 
-    async save(object) {
-        // ? Recibe un objeto, lo guarda en el archivo y devuelve el id asignado
+app.set("port", 8080); //*Configuración puerto
 
-        const dataToParse = await fs.readFileSync(this.file, "utf-8");
-        const dataParsed = JSON.parse(dataToParse);
-        // * ¿El producto ya existe en el archivo?
-        const productFound = dataParsed.find(({
-            title
-        }) => title == object.title);
+//? Routes
 
-        try {
-            if (productFound) {
-                // * Si el producto ya existe, avisa por consola y no lo agrega
-                console.log("El producto ya existe en el archivo");
-            } else {
-                // * Si no existe, lo agrega y retorna el id asignado
-                object.id = dataParsed.length + 1;
-                dataParsed.push(object);
-                const updatedFile = JSON.stringify(dataParsed, null, " ");
-                fs.writeFileSync(this.file, updatedFile);
-                // ! console.log(updatedFile, "Aquí 23");
-                console.log(
-                    `Se ha agregado el siguiente producto: ${object.title} con el id ${object.id}`
-                );
-                return object.id;
-            }
-        } catch (error) {
-            console.error(`Se produjo un error en save:${error}`);
-        }
-    }
+app.get("/", (req, res) => {
+    res.send("<h1 style='color:blue'>HOLA SERVIDOR</h1>");
+});
 
-    async getById(idEntered) {
-        // ? Recibe un id y devuelve el objeto con ese id, o null si no está
+app.get("/products", async (req, res) => {
+    let data = await contenedor.getAll();
+    res.send(data);
+});
 
-        const dataToParse = await fs.readFileSync(this.file, "utf-8");
-        const dataParsed = JSON.parse(dataToParse);
-        // * ¿El producto ya existe en el archivo?
-        const idFound = dataParsed.find(({
-            id
-        }) => id === idEntered);
+app.get("/randomProduct", async (req, res) => {
+    //! Números aleatorios del 1 al 10
+    let randomNum = Math.floor(Math.random() * 9 + 1);
+    let data = await contenedor.getById(randomNum);
+    //! Si el id generado no coincide con ningún producto, devuelve null; de lo contrario, envía la información solicitada
+    data === null ?
+        res.send(`<h4>ID:${randomNum} >> [[ERROR]] No se ha encontrado el producto</h4>`) :
+        res.json(data);
+});
 
-        try {
-            if (idFound) {
-                console.table(idFound);
-                return idFound;
-            } else {
-                console.log("No se ha encontrado el producto");
-                return null;
-            }
-        } catch (error) {
-            console.error(`Se produjo un error en getByID: ${error}`);
-        }
-    }
 
-    async getAll() {
-        // ? Devuelve un array con los objetos presentes en el archivo
+//? Servidor iniciado
 
-        const dataToParse = await fs.readFileSync(this.file, "utf-8");
-        const dataParsed = JSON.parse(dataToParse);
+const server = app.listen(app.get("port"), () => {
+    console.log(`Servidor express iniciado en puerto ${app.get("port")}`);
+});
 
-        try {
-            if (dataParsed.length > 0) {
-                console.log(dataParsed);
-                return dataParsed;
-            } else {
-                console.log("No hay elementos disponibles");
-            }
-        } catch (error) {
-            console.error(`Se ha producido un error en getAll: ${error}`);
-        }
-    }
+//? Manejo de errores
 
-    async deleteById(idEntered) {
-        // ? Elimina del archivo el objeto con el Id buscado
-
-        const dataToParse = await fs.readFileSync(this.file, "utf-8");
-        const dataParsed = JSON.parse(dataToParse);
-        // * Se filtran los productos que no cumplen las condiciones (coincidir con el id proporcionado)
-        const leakedID = dataParsed.filter(({
-            id
-        }) => id !== idEntered);
-        // * Encuentra el producto con el id proporcionado
-        const idFound = dataParsed.find(({
-            id
-        }) => id === idEntered);
-
-        try {
-            if (idFound) {
-                console.log(
-                    `Se ha eliminado el objeto con id:${idEntered} >> [[${idFound.title}]]`
-                );
-                // * Se actualiza el archivo
-                const updatedFile = JSON.stringify(leakedID, null, " ");
-                fs.writeFileSync(this.file, updatedFile);
-            } else {
-                console.log(`No se ha encontrado el objeto con id: ${idEntered}`);
-            }
-        } catch (error) {
-            console.error(`Se ha producido un error en deleteById: ${error}`);
-        }
-    }
-
-    async deleteAll() {
-        // ? Elimina todos los objetos presentes en el archivo
-        try {
-            console.log("Todos los objetos fueron eliminados");
-            // * Borrado de todos los objetos (Se sobreescribe el archivo a un array vacío)
-            await fs.writeFileSync(this.file, "[]");
-        } catch (error) {
-            console.error(`Se ha producido un error en deleteAll: ${error}`);
-        }
-    }
-}
-
-const file = "./products.json";
-const contenedor = new Container(file);
-
-let newObject = {
-    title: "pantalon",
-    price: 74500,
-    thumbnail: "pantalon.negro",
-};
-
-let anotherObject = {
-    title: "zapatillas",
-    price: 50000,
-    thumbnail: "zapatillas.negro",
-};
-
-//TODO Descomentar para correr las funciones
-
-contenedor.save(newObject);
-contenedor.save(anotherObject);
-// contenedor.getById(3)
-// contenedor.getAll();
-// contenedor.deleteById(4)
-// contenedor.deleteAll()
+server.on("error", (error) => {
+    console.log(`Error !!!: ${error}`);
+});
