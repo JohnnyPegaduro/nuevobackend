2e5ec54499c785276fc7903b20cbbcd2a714c187
JohnnyPegaduro <matiasdeluca2000@gmail.com>
2022-09-22 10:41:28 -0300

    agregando estilos y chat

---
+++ public/chat.js
@@ -0,0 +1,57 @@
+const socket = io();
+
+//!  DOM ELEMENTS
+
+let message = document.getElementById("message");
+let username = document.getElementById("username");
+let btn = document.getElementById("send");
+let output = document.getElementById("output");
+let actions = document.getElementById("actions");
+
+btn.addEventListener("click", () => {
+    //TODO Arreglar CHAT
+    socket.emit("chat:message", {
+        username: username.value,
+        message: message.value,
+    });
+});
+
+function render(data) {
+
+    const html = data
+        .map((msg) => {
+            `<p>
+    <strong class="message-user">${
+    msg.username
+    } <span class="message-date">[ ${new Date().toLocaleString()} ]</span></strong>: <span class="message-txt">${
+        msg.message
+    }</span>
+    </p>`;
+        })
+        .join(" ");
+
+    output.innerHTML = html;
+} //ToDO Function debe implementarse???
+
+message.addEventListener("keypress", () => {
+    socket.emit("chat:typing", username.value);
+});
+
+socket.on("all:messages", (data) => {
+    render(data);
+}); //TODO Traer todo el array de mensajes
+
+socket.on("chat:message", (data) => {
+    actions.innerHTML = " ";
+    output.innerHTML += `<p>
+    <strong class="message-user">${
+    data.username
+    } <span class="message-date">[ ${new Date().toLocaleString()} ]</span></strong>: <span class="message-txt">${
+    data.message
+    }</span>
+    </p>`;
+});
+
+socket.on("chat:typing", (data) => {
+    actions.innerHTML = `<p></em>${data} is typing a message </p>`;
+});
