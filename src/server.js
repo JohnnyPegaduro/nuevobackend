8f4ceab22e6f908e2f9d5b54cbe3aec24d1f5dc6
JohnnyPegaduro <matiasdeluca2000@gmail.com>
2022-09-23 05:22:49 -0300

    websocket server y products

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const server = app.listen(app.get("port"), () => {
     console.log(`Servidor express iniciado en puerto ${app.get("port")}`);
 });
 
-
 //! ERROR HANDLER
 
 server.on("error", (error) => {
@@ -17,28 +16,49 @@ server.on("error", (error) => {
 
 const SocketIO = require("socket.io");
 const io = SocketIO(server);
-const Messages = [{
-        username: "Matias",
-        message: "HOla"
-    },
-    {
-        username: "Miguel",
-        message: "HOlaa jesus"
-    },
-    {
-        username: "Fabiola",
-        message: "Hola Amoor"
-    },
-]; //TODO Arreglar CHAT
+//! CONTENEDOR PRODUCTOS
+const Container = require("./models/Container");
+const contenedor = new Container("products.json");
+//! CONTENEDOR PRODUCTOS
+const Messages = []; //TODO Cambiar a archivo json
 
-io.on("connection", (socket) => {
+console.log(Messages)
+
+
+io.on("connection", async (socket) => {
+
+    //! Nueva conexión
     console.log(`New Connection: ${socket.id}`);
-    socket.emit("all:messages", Messages); //todo evento allmessages nuevo : debe implementarse?
+
+    //! PRODUCTOS
+
+    const products = await contenedor.getAll()
+    io.sockets.emit("get-products", products);
+
+    socket.on("new-product", async (object) => {
+        //! Guardar producto
+        const data = await contenedor.save(object);
+        data === null ?
+            socket.emit("new-product", object) :
+            socket.emit("new-product", data);
+    });
+
+
+
+    //! CHAT
+
+    //! El evento chat:messages iniciará enviando el array existente al cliente
+    socket.emit("chat:messages", Messages);
+
+
+    //! Se escucha el evento chat:message, se guarda el mensaje recibido por el cliente y se emite un mensaje general con el array Messages actualizado a todos los sockets conectados y por conectarse
 
     socket.on("chat:message", (data) => {
-        Messages.push(data); //FIXME me hago cargo de esto?
-        io.sockets.emit("chat:message", data); //! Enviar nuevos mensajes
+        Messages.push(data);
+        io.sockets.emit("chat:messages", Messages);
     });
+
+    //! Se escucha el evento chat:typing y se emite un mensaje a todos los sockets conectados, excepto al que "está escribiendo..." con el método broadcast
 
     socket.on("chat:typing", (data) => {
         socket.broadcast.emit("chat:typing", data);
